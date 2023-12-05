91c4e96c74893b5bd992f4199b7b1b61f19d4eb1
Maxr1998 <max.rumpf1998@gmail.com>
2023-12-05 15:39:40 +0100

    :wrench: Set up publishing configuration

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,24 +1,41 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    `java-gradle-plugin`
+    alias(libs.plugins.gradle.publish)
+    signing
 }
 
 object PluginInfo {
     const val GROUP = "de.maxr1998"
     const val ID = "$GROUP.proguard-dictionaries-generator"
     const val IMPLEMENTATION_CLASS = "$GROUP.plugin.ProguardR8DictionaryGeneratorPlugin"
+    const val GIT_REPO = "https://github.com/Maxr1998/Proguard-Dictionaries-Generator"
 }
 
 group = PluginInfo.GROUP
 version = libs.versions.proguarddictionariesgenerator.get()
 
 gradlePlugin {
+    website = PluginInfo.GIT_REPO
+    vcsUrl = "${PluginInfo.GIT_REPO}.git"
+
     plugins {
-        create("plugin") {
-            version = libs.versions.proguarddictionariesgenerator.get()
+        create("proguardDictionariesGeneratorPlugin") {
             id = PluginInfo.ID
             displayName = "Proguard R8 Dictionaries Generator Plugin"
+            description = "Gradle Plugin that generates randomized dictionaries for ProGuard/R8"
+            version = libs.versions.proguarddictionariesgenerator.get()
             implementationClass = PluginInfo.IMPLEMENTATION_CLASS
+
+            tags = listOf(
+                "android",
+                "dictionary",
+                "generator",
+                "minification",
+                "obfuscation",
+                "proguard",
+                "R8",
+                "shrinking",
+            )
         }
     }
 }
@@ -26,4 +43,9 @@ gradlePlugin {
 dependencies {
     compileOnly(gradleApi())
     compileOnly(libs.android.tools)
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
